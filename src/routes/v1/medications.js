6e4b4bd98d14be37ef1fcfbfb15d0946aae96908
Audrius <kurgonas.a@gmail.com>
2022-08-28 22:18:32 +0300

    fixed medications table

--- src/routes/v1/medications.js
+++ src/routes/v1/medications.js
@@ -1,52 +1,86 @@
 const express = require('express')
-const fetch = require('node-fetch')
-const isLoggedIn = require('../../middleware/auth')
-const { addMedSchema } = require('../../middleware/schemas/medicationSchema')
-const validation = require('../../middleware/validation')
-const { strapiUser, strapiPass, strapiServer } = require('../../config')
+const mysql = require('mysql2/promise')
+const { mysqlConfig } = require('../../config')
 
 const router = express.Router()
 
-router.post(
-  '/add_medication',
-  isLoggedIn,
-  validation(addMedSchema),
-  async (req, res) => {
-    try {
-      const response = await fetch(`${strapiServer}/api/auth/local/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          identifier: strapiUser,
-          password: strapiPass
-        })
-      })
-      const data = await response.json()
-
-      const response2 = await fetch(`${strapiServer}/api/medications`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: `Bearer ${data.jwt}`
-        },
-        body: JSON.stringify({
-          data: {
-            title: req.body.title,
-            description: req.body.description,
-            price: req.body.price
-          }
-        })
-      })
-      const data2 = await response2.json()
-
-      return res.status(200).send(data2)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).send({ msg: 'Server issue.' })
-    }
+// Get all medications
+router.get('/', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(mysqlConfig)
+    const [data] = await connection.execute(`
+    SELECT * FROM medications
+    `)
+    await connection.end()
+    return res.send(data)
+  } catch (err) {
+    return res.status(500).send({ err: 'Server issue...' })
   }
-)
+})
+
+// Add medication
+router.post('/add_medication', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(mysqlConfig)
+    const [data] = await connection.execute(`
+    INSERT INTO medications (title, description, price)
+    VALUES (${mysql.escape(req.body.title)}, ${mysql.escape(
+      req.body.description
+    )}, ${mysql.escape(req.body.price)})
+    `)
+    await connection.end()
+
+    if (data.affectedRows !== 1) {
+      await connection.end()
+      return res.status(500).send({ err: 'Server issue' })
+    }
+
+    return res.status(200).send({ msg: 'Successfully added a medication.' })
+  } catch (err) {
+    return res.status(500).send({ err: 'Server issue...' })
+  }
+})
+
+// router.post(
+//   '/add_medication',
+//   isLoggedIn,
+//   validation(addMedSchema),
+//   async (req, res) => {
+//     try {
+//       const response = await fetch(`${strapiServer}/api/auth/local/`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//           identifier: strapiUser,
+//           password: strapiPass
+//         })
+//       })
+//       const data = await response.json()
+
+//       const response2 = await fetch(`${strapiServer}/api/medications`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           authorization: `Bearer ${data.jwt}`
+//         },
+//         body: JSON.stringify({
+//           data: {
+//             title: req.body.title,
+//             description: req.body.description,
+//             price: req.body.price
+//           }
+//         })
+//       })
+//       const data2 = await response2.json()
+
+//       return res.status(200).send(data2)
+//     } catch (err) {
+//       console.log(err)
+//       return res.status(500).send({ msg: 'Server issue.' })
+//     }
+//   }
+// )
 
 module.exports = router
